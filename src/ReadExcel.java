d6697730e946e3339d9e93ec1097787cca6d59ee
JoelFranklin <joel.franklin@csscorp.com>
2017-03-29 13:28:42 +0530

    done formatting

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -11,43 +11,34 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ReadExcel {
-	
-	
-	public static void readXLSFile() throws IOException, InvalidFormatException
-	{
+
+	public static void readXLSFile() throws IOException, InvalidFormatException {
 		InputStream ExcelFileToRead = new FileInputStream("D:/country.xls");
 		Workbook wb = WorkbookFactory.create(ExcelFileToRead);
-		Sheet sheet=wb.getSheetAt(0);
-		Row row; 
+		Sheet sheet = wb.getSheetAt(0);
+		Row row;
 		Cell cell;
 
 		Iterator rows = sheet.rowIterator();
 
-		while (rows.hasNext())
-		{
-			row=(Row) rows.next();
+		while (rows.hasNext()) {
+			row = (Row) rows.next();
 			Iterator cells = row.cellIterator();
-			
-			while (cells.hasNext())
-			{
-				cell=(Cell) cells.next();
-		
-				if (cell.getCellType() == Cell.CELL_TYPE_STRING)
-				{
-					System.out.print(cell.getStringCellValue()+" ");
-				}
-				else if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC)
-				{
-					System.out.print(cell.getNumericCellValue()+" ");
-				}
-				else
-				{
-					//U Can Handel Boolean, Formula, Errors
+
+			while (cells.hasNext()) {
+				cell = (Cell) cells.next();
+
+				if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+					System.out.print(cell.getStringCellValue() + " ");
+				} else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+					System.out.print(cell.getNumericCellValue() + " ");
+				} else {
+					// U Can Handel Boolean, Formula, Errors
 				}
 			}
 			System.out.println();
 		}
-	
+
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -57,7 +48,7 @@ public class ReadExcel {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
